78f9aecf9d76b67cd719c94bfd5bf82f585e7106
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-08-03 17:00:02 +0200

    Finished projects components without slider

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const ProjectCard = ({ children, header, img, link }) => {
   return (
     <a href={link} target="_blank">
-      <div className="m-2 w-80 rounded-lg bg-white transition-shadow duration-300 hover:shadow-2xl">
+      <div className="m-2 w-80 rounded-lg bg-white transition-shadow duration-300 hover:shadow-md">
         <h3 className="p-3 text-3xl font-bold">{header}</h3>
         <Image
           src={"/" + img}
--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -4,10 +4,10 @@ const Projects = () => {
   return (
     <article className="mt-4 p-8 text-center rounded-xl bg-dark-sea-green/50">
       <h2 className="mb-8 text-6xl font-bold">Projects</h2>
-      <div className="flex justify-center">
-        <ProjectCard header="Project X" img="placeholder.jpg" link="https://tailwindcss.com/docs/transition-property">Project X description</ProjectCard>
-        <ProjectCard header="Project Y" img="placeholder.jpg">Project Y description</ProjectCard>
-        <ProjectCard header="Project Z" img="placeholder.jpg">Project Z description</ProjectCard>
+      <div className="flex justify-center p-3 rounded-xl bg-dark-sea-green/50">
+        <ProjectCard header="Minesweeper" img="placeholder.jpg" link="https://github.com/matsbak/minesweeper">
+          TBD! Classic minesweeper desktop app coded in Java
+        </ProjectCard>
       </div>
     </article>
   );
