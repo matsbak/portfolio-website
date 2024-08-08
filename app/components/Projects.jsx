0e8727f2a88c41a69b410359ce5f4d45e883251e
Mats Bakketeig <matsbak@stud.ntnu.no>
2024-08-08 20:59:34 +0200

    Added scrollbar to projects component

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
     <article className="mt-14 p-8 rounded-xl text-center bg-dark-sea-green/50">
       <h2 className="text-6xl font-bold">Projects</h2>
       <p className="mt-8">Projects I have worked on.</p>
-      <div className="flex justify-center mt-8 p-3 rounded-xl bg-white">
+      <div className="flex justify-center mt-8 p-3 overflow-auto rounded-xl bg-white">
         <ProjectCard header="Minesweeper" img="placeholder.jpg" link="https://github.com/matsbak/minesweeper">
           TBD! Classic minesweeper desktop app coded in Java
         </ProjectCard>
